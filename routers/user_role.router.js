15331f4ab0dcdffaa858bd5021e10b2881df2aca
thinhvd <thinhvd@ghn.vn>
2020-07-31 17:37:10 +0700

    add protect to change pass

--- routers/user_role.router.js
+++ routers/user_role.router.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const { getMe } = require('../controllers/auth.controller');
-const {changePassword, createUser, getInfoUser, getPartnerInfo, updateInfoUser, deleteInfoUser, getInfoUserBy} = require('../controllers/user_role.controller');
+const {changePassword, createUser, getInfoUser, getPartnerInfo, updateInfoUser, deleteInfoUser, getInfoUserBy, resetPassword} = require('../controllers/user_role.controller');
 
 const router = express.Router();
 
 const { protect } = require('../middlewares/auth.middleware');
 
-router.post('/change-password', changePassword);
+router.post('/change-password', protect, changePassword);
 router.post('/create', protect, createUser);
 router.get('/get-info', protect, getInfoUser);
 router.get('/get-partner', protect, getPartnerInfo);
 router.put('/update', protect, updateInfoUser);
 router.delete('/delete', protect, deleteInfoUser);
 router.get('/get-info-user-by', getInfoUserBy);
+router.post('/reset', resetPassword)
 module.exports = router;
