10b6a891879045142d05f6e8ee8c058456e21e7b
thinhvd <thinhvd@ghn.vn>
2020-08-01 17:20:47 +0700

    add api get bank account

--- routers/bank_account.router.js
+++ routers/bank_account.router.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { createBankAccount, transferMoney} = require('../controllers/bank_account.controller');
+const { createBankAccount, transferMoney, getBankAccountDeposit, getBankAccountStandard} = require('../controllers/bank_account.controller');
 const { protect } = require('../middlewares/auth.middleware');
 
 const router = express.Router();
 
 router.post('/create', protect, createBankAccount);
 router.post('/transfer', protect, transferMoney);
+router.get('/standard', getBankAccountStandard);
+router.get('/deposit', getBankAccountDeposit);
 
 module.exports = router;
