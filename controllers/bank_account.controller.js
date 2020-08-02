6acc529b6845ad13417b1e0cb5adaef8004347b3
thinhvd <thinhvd@ghn.vn>
2020-08-03 00:54:00 +0700

    update

--- controllers/bank_account.controller.js
+++ controllers/bank_account.controller.js
@@ -29,12 +29,13 @@ const getBankAccountDeposit = async(req, res, next) => {
     let limit = parseInt(req.query.limit);
     let accountNumber = req.query.accountNumber;
     filter = {
-        user_id: req.user.user_id
+        user_id: req.user.user_id,
+        type: DEPOSIT_ACCOUNT
     }
     if (accountNumber){
         filter['account_number'] = accountNumber
     }
-    let bankAccount = await BankAccountDB.find({user_id: req.user.user_id, type: DEPOSIT_ACCOUNT}).limit(limit ? limit : 20).skip(offset ? offset : 0)
+    let bankAccount = await BankAccountDB.find(filter).limit(limit ? limit : 20).skip(offset ? offset : 0)
     if (bankAccount){
         return res.status(200).json({
             message: "Get bank deposit account successful.",
